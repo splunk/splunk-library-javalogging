a6a1b2d79c9463dee911f2667fafdee845ff1189
DalliD <DalliD@6930PSGH008RPY7.corp.ad.airnz.co.nz>
2012-02-11 19:18:45 +1300

    Change-Id: Ie13ef5292bfa3aeaa02be9ebc8bade91080a2150

--- src/com/dtdsoftware/splunk/logging/SplunkRestInput.java
+++ src/com/dtdsoftware/splunk/logging/SplunkRestInput.java
@@ -5,14 +5,9 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.Socket;
-import java.security.cert.X509Certificate;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
-import com.splunk.RequestMessage;
+import com.splunk.Args;
+import com.splunk.Receivers;
 import com.splunk.Service;
 
 /**
@@ -24,142 +19,118 @@ import com.splunk.Service;
 
 public class SplunkRestInput {
 
-	public static String RECEIVERS_SIMPLE_ENDPOINT = "/services/receivers/simple";
-	public static String RECEIVERS_STREAM_ENDPOINT = "/services/receivers/stream";
 
-	public static String RECEIVERS_SIMPLE_ARG_INDEX = "index";
-	public static String RECEIVERS_SIMPLE_ARG_SOURCE = "source";
-	public static String RECEIVERS_SIMPLE_ARG_SOURCETYPE = "sourcetype";
-	public static String RECEIVERS_SIMPLE_ARG_HOST = "host";
-	public static String RECEIVERS_SIMPLE_ARG_HOSTREGEX = "host_regex";
-
-	public static String SCHEME_HTTPS = "https";
-	public static String SCHEME_HTTP = "http";
-
-	public static String HTTP_METHOD = "POST";
-
+	// Java SDK objects
 	private Service service;
+	private Receivers receivers;
+	private Args args;
 
+	// streaming objects
+	private Socket streamSocket = null;
 	private OutputStream ostream;
-	private Socket stream = null;
 	private Writer writerOut = null;
-	private String scheme = SCHEME_HTTPS;
 
-	private RestEventData red;
-	private String simpleURL;
-
-	private TrustManager[] trustAll = new TrustManager[] { new X509TrustManager() {
-		public X509Certificate[] getAcceptedIssuers() {
-			return null;
-		}
-
-		public void checkClientTrusted(X509Certificate[] certs, String authType) {
-		}
-
-		public void checkServerTrusted(X509Certificate[] certs, String authType) {
-		}
-	} };
-
+	/**
+	 * Create a SplunkRestInput object to send events to Splunk via REST
+	 * endpoints
+	 * 
+	 * @param user
+	 *            Splunk user
+	 * @param pass
+	 *            Splunk user pass
+	 * @param host
+	 *            REST endppoint host
+	 * @param port
+	 *            REST endpoint port
+	 * @param red
+	 *            url parameter values
+	 * @param stream
+	 *            true=stream , false=do not stream (be aware, poor performance)
+	 * @throws Exception
+	 */
 	public SplunkRestInput(String user, String pass, String host, int port,
 			RestEventData red, boolean stream) throws Exception {
 
 		this.service = new Service(host, port);
 		this.service.login(user, pass);
-		this.red = red;
-		this.simpleURL = getURL(RECEIVERS_SIMPLE_ENDPOINT, red);
-		if (stream)
-			initStream();
+		this.receivers = new Receivers(this.service);
+
+		this.args = createArgs(red);
+
+		if (stream) {
+			this.streamSocket = this.receivers.attach(args);
+			ostream = streamSocket.getOutputStream();
+			writerOut = new OutputStreamWriter(ostream, "UTF8");
+		}
 
 	}
 
-	private void initStream() throws Exception {
+	/**
+	 * Create a SDK Args object from a RestEventData object
+	 * 
+	 * @param red
+	 * @return
+	 */
+	private Args createArgs(RestEventData red) {
 
-		this.stream = open(service.getHost(), service.getPort());
-		this.ostream = stream.getOutputStream();
-		this.writerOut = new OutputStreamWriter(this.ostream, "UTF8");
+		Args urlArgs = new Args();
 
-		String header = String.format("%s %s HTTP/1.1\r\n" + "Host: %s:%d\r\n"
-				+ "Accept-Encoding: identity\r\n" + "Authorization: %s\r\n"
-				+ "X-Splunk-Input-Mode: Streaming\r\n\r\n", HTTP_METHOD,
-				getURL(RECEIVERS_STREAM_ENDPOINT, red), service.getHost(),
-				service.getPort(), service.getToken());
+		if (red != null) {
+			if (red.getIndex().length() > 0)
+				urlArgs.add(RestEventData.RECEIVERS_SIMPLE_ARG_INDEX, red.getIndex());
+			if (red.getSource().length() > 0)
+				urlArgs.add(RestEventData.RECEIVERS_SIMPLE_ARG_SOURCE, red.getSource());
+			if (red.getSourcetype().length() > 0)
+				urlArgs.add(RestEventData.RECEIVERS_SIMPLE_ARG_SOURCETYPE, red
+						.getSourcetype());
+			if (red.getHost().length() > 0)
+				urlArgs.add(RestEventData.RECEIVERS_SIMPLE_ARG_HOST, red.getHost());
+			if (red.getHostRegex().length() > 0)
+				urlArgs.add(RestEventData.RECEIVERS_SIMPLE_ARG_HOSTREGEX, red.getHostRegex());
 
-		this.writerOut.write(header);
-		this.writerOut.flush();
-
+		}
+		return urlArgs;
 	}
 
+	/**
+	 * close the stream
+	 */
 	public void closeStream() {
 		try {
-			writerOut.flush();
-			writerOut.close();
-			stream.close();
-		} catch (IOException e) {
-
+			if (writerOut != null) {
+				writerOut.flush();
+				writerOut.close();
+				if (streamSocket != null)
+					streamSocket.close();
+			}
+		} catch (Exception e) {
 		}
 	}
 
 	/**
-	 * Open a socket to this service.
+	 * send a single event , not so good for performance, streaming is better
 	 * 
-	 * @return Socket
-	 * @throws IOException
+	 * @param message
 	 */
-	private Socket open(String host, int port) throws IOException {
-		if (this.scheme.equals("https")) {
-			SSLSocketFactory sslsocketfactory;
-			try {
-				SSLContext context = SSLContext.getInstance("SSL");
-				context.init(null, trustAll, new java.security.SecureRandom());
-				sslsocketfactory = context.getSocketFactory();
-			} catch (Exception e) {
-				throw new RuntimeException("Error installing trust manager.");
-			}
-			return sslsocketfactory.createSocket(host, port);
-		}
-		return new Socket(host, port);
-	}
-
 	public void sendEvent(String message) {
 
-		RequestMessage request = new RequestMessage(HTTP_METHOD);
-		request.setContent(message);
-		service.send(simpleURL, request);
+		if (streamSocket == null)
+			this.receivers.submit(message, args);
 	}
 
-	private String getURL(String endpoint, RestEventData red) {
-
-		StringBuffer url = new StringBuffer();
-
-		url.append(endpoint).append("?");
-
-		if (red.getIndex().length() > 0)
-			url.append(RECEIVERS_SIMPLE_ARG_INDEX).append("=").append(
-					red.getIndex());
-		if (red.getSource().length() > 0)
-			url.append("&").append(RECEIVERS_SIMPLE_ARG_SOURCE).append("=")
-					.append(red.getSource());
-		if (red.getSourcetype().length() > 0)
-			url.append("&").append(RECEIVERS_SIMPLE_ARG_SOURCETYPE).append("=")
-					.append(red.getSourcetype());
-		if (red.getHost().length() > 0)
-			url.append("&").append(RECEIVERS_SIMPLE_ARG_HOST).append("=")
-					.append(red.getHost());
-		if (red.getHostRegex().length() > 0)
-			url.append("&").append(RECEIVERS_SIMPLE_ARG_HOSTREGEX).append("=")
-					.append(red.getHostRegex());
-
-		return url.toString();
-
-	}
-
+	/**
+	 * send an event via stream
+	 * 
+	 * @param message
+	 */
 	public void streamEvent(String message) {
 
 		try {
-			writerOut.write(message + "\n");
-			writerOut.flush();
+			if (writerOut != null) {
+				writerOut.write(message + "\n");
+				writerOut.flush();
+			}
 		} catch (IOException e) {
-
 		}
 
 	}

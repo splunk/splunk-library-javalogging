ec075487917fc35f4dd71eeafdd61cfc1c5fe888
Damien Dallimore <damien@dtdsoftware.com>
2012-05-10 08:26:48 +1200

    Change-Id: I224d394faee4fd4d3d7081bf8c58c82f23cb45b2

--- src/com/dtdsoftware/splunk/logging/SplunkLogEventFactory.java
+++ src/com/dtdsoftware/splunk/logging/SplunkLogEventFactory.java
@@ -4,8 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * A factory class that allows you to set SplunkLogEvent templates and generate clones.
- * Useful for reuse when you log many events with the same key=value pair signatures.
+ * A factory class that allows you to set SplunkLogEvent templates and generate
+ * clones. Useful for reuse when you log many events with the same key=value
+ * pair signatures.
  * 
  * <code>
  * 
@@ -16,48 +17,51 @@ import java.util.Map;
  * event.addPair("someparam", "helloworld");
  * 
  * </code>
- * 	
+ * 
  * @author Damien Dallimore damien@dtdsoftware.com
- *
+ * 
  */
 public abstract class SplunkLogEventFactory {
-	
-	//map to hold the templates
-	private static Map<String,SplunkLogEvent>templates = new HashMap<String,SplunkLogEvent>();
-	
+
+	// map to hold the templates
+	private static Map<String, SplunkLogEvent> templates = new HashMap<String, SplunkLogEvent>();
+
 	/**
 	 * Add a template SplunkLogEvent object
+	 * 
 	 * @param name
 	 * @param template
 	 */
-	public static void addTemplate(String name, SplunkLogEvent template){
-		
+	public static void addTemplate(String name, SplunkLogEvent template) {
+
 		templates.put(name, template);
 	}
-	
+
 	/**
 	 * Remove a template SplunkLogEvent object
+	 * 
 	 * @param name
 	 */
-	public static void removeTemplate(String name){
-		
+	public static void removeTemplate(String name) {
+
 		templates.remove(name);
 	}
-	
+
 	/**
 	 * Get a new SplunkLogEvent object that is a clone of a template
+	 * 
 	 * @param name
 	 * @return
 	 * @throws Exception
 	 */
-    public static SplunkLogEvent getInstanceFromTemplate(String name) throws Exception{
-		
-    	SplunkLogEvent template = templates.get(name);
-    	if(template != null){
-    		return template.clone();
-    	}
-    	else
-    		throw new Exception("SplunkLogEvent template "+name+" does not exist");
+	public static SplunkLogEvent getInstanceFromTemplate(String name)
+			throws Exception {
+
+		SplunkLogEvent template = templates.get(name);
+		if (template != null) {
+			return template.clone();
+		} else
+			throw new Exception("SplunkLogEvent template " + name
+					+ " does not exist");
 	}
 }
-

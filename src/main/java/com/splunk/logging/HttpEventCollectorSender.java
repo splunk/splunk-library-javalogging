4bab53001c0c2841130b0b5b8e5d15f0071d7906
Ajay Deshwal <ajay.deshwal@tsbbank.co.nz>
2017-05-05 08:04:16 +1200

    Extracted MessageFormat into a separate type file. Encapsulated parsing methods into MessageFormat enum.

--- src/main/java/com/splunk/logging/HttpEventCollectorSender.java
+++ src/main/java/com/splunk/logging/HttpEventCollectorSender.java
@@ -30,7 +30,6 @@ import org.apache.http.impl.nio.client.HttpAsyncClients;
 import org.apache.http.util.EntityUtils;
 
 import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 
 import javax.net.ssl.SSLContext;
 import java.io.IOException;
@@ -74,44 +73,6 @@ final class HttpEventCollectorSender extends TimerTask implements HttpEventColle
         Parallel
     };
     
-    /**
-     * 
-     * Format of the event message. It is used to apply necessary parsing and formatting to the message, if required for the
-     * message format.
-     * <p>
-     * If format is not set, then it is assumed to be "text"
-     * </p>
-     *
-     */
-    public enum MessageFormat {
-        
-        TEXT("text"),
-        JSON("json");
-
-        private final String format;
-
-        MessageFormat(final String format) {
-            this.format = format;
-        }
-
-        /**
-         * Gets MessageFormat instance from string format.
-         *
-         * @param format the message format
-         * @return the message format
-         */
-        public static MessageFormat fromFormat(final String format) {
-            if (format != null && format.trim().length() > 0) {
-                for (final MessageFormat formatEnum : values()) {
-                    if (formatEnum.format.equals(format)) {
-                        return formatEnum;
-                    }
-                }
-            }
-            return TEXT;
-        }
-    };
-
     /**
      * Recommended default values for events batching.
      */
@@ -275,7 +236,7 @@ final class HttpEventCollectorSender extends TimerTask implements HttpEventColle
         putIfPresent(event, MetadataSourceTypeTag, metadata.get(MetadataSourceTypeTag));
         
         // Parse message on the basis of format
-        final Object parsedMessage = parseEventMessage(eventInfo.getMessage(), this.messageFormat);
+        final Object parsedMessage = this.messageFormat.parse(eventInfo.getMessage());
         
         // event body
         JSONObject body = new JSONObject();
@@ -302,48 +263,6 @@ final class HttpEventCollectorSender extends TimerTask implements HttpEventColle
         // join event and body
         event.put("event", body);
         return event.toString();
-    }
-
-    /**
-     * Parses the event message on the basis of message format.
-     * 
-     * @param message the event message string
-     * @param format the event message format
-     * 
-     * @return parsed event message based on format
-     */
-    private Object parseEventMessage(final String message, final MessageFormat messageFormat) {
-        // if message is null or blank then return without parsing
-        if (message == null || message.trim().length() == 0) {
-            return message;
-        }
-
-        switch (messageFormat) {
-            case JSON:
-                return parseJsonEventMessage(message);
-
-            case TEXT:
-            default:
-                // Treat message type as text if format is not defined or defined as text
-                return message;
-        }
-    }
-
-    /**
-     * Parses the event message JSON string into JSON object. If parsing fails then the input message is returned as is.
-     *
-     * @param message the event message string
-     * @return the parsed event message JSON object or input message if parsing fails
-     */
-    private Object parseJsonEventMessage(final String message) {
-        final Object jsonObject = JSONValue.parse(message);
-        if (jsonObject == null) {
-            // If JSON parsing failed then it is likely a text message or a malformed JSON message. Return input message string in
-            // such an event.
-            return message;
-        } else {
-            return jsonObject;
-        }
     }
 
     private void startHttpClient() {
---
+++ src/main/java/com/splunk/logging/MessageFormat.java
@@ -0,0 +1,88 @@
+package com.splunk.logging;
+
+import org.json.simple.JSONValue;
+
+/**
+ * 
+ * Represents format of a message. It is used to apply necessary parsing and formatting to the message, if required for
+ * the message format.
+ * <p>
+ * If format is not set, then it is assumed to be "text"
+ * </p>
+ * <p>
+ * Note: Access modifier is intentionally left default which allows this enum to be accessed only within the package.
+ * </p>
+ *
+ */
+enum MessageFormat {
+
+    TEXT("text"),
+    JSON("json");
+
+    private final String format;
+
+    MessageFormat(final String format) {
+        this.format = format;
+    }
+
+    /**
+     * Parses the message on the basis of message format.
+     * 
+     * 
+     * @param message the message string
+     * @param format the message format
+     * 
+     * @return parsed message object based on format
+     */
+    Object parse(final String message) {
+        // if message is null or blank then return without parsing
+        if (message == null || message.trim().length() == 0) {
+            return message;
+        }
+
+        switch (this) {
+            case JSON:
+                return parseJsonEventMessage(message);
+
+            case TEXT:
+            default:
+                // Treat message type as text if format is not defined or defined as text
+                return message;
+        }
+    }
+
+    /**
+     * Parses the message JSON string into JSON object. If parsing fails then the input message is returned as is.
+     *
+     * @param message the message string
+     * @return the parsed message JSON object or input message if parsing fails
+     */
+    private Object parseJsonEventMessage(final String message) {
+        final Object jsonObject = JSONValue.parse(message);
+        if (jsonObject == null) {
+            // If JSON parsing failed then it is likely a text message or a malformed JSON message.
+            // Return input message string in such an event.
+            return message;
+        } else {
+            return jsonObject;
+        }
+    }
+
+    /**
+     * Gets MessageFormat instance from format string.
+     *
+     * @param format the message format
+     * @return the MessageFormat enum
+     */
+    static MessageFormat fromFormat(String format) {
+        if (format != null && format.trim().length() > 0) {
+            format = format.toLowerCase();
+            for (final MessageFormat formatEnum : values()) {
+                if (formatEnum.format.equals(format)) {
+                    return formatEnum;
+                }
+            }
+        }
+        return TEXT;
+    }
+}

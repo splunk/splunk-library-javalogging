5f8569bc6d7780d82eccbe4a39382146a3fcc10b
Alexander Pavlov <alexander.pavlov@ihsmarkit.com>
2021-03-18 14:45:42 -0400

    Code review

--- src/main/java/com/splunk/logging/serialization/HecJsonSerializer.java
+++ src/main/java/com/splunk/logging/serialization/HecJsonSerializer.java
@@ -48,8 +48,9 @@ public class HecJsonSerializer {
         Map<String, Object> event = new HashMap<>(template);
         if (this.eventBodySerializer != null) {
             event.put("event", eventBodySerializer.serializeEventBody(info, info.getMessage()));
-            if (info.getTime() > 0) {
-                event.put("time", String.format(Locale.US, "%.3f", info.getTime()));
+            double eventTime = eventBodySerializer.getEventTime(info);
+            if (eventTime > 0) {
+                event.put("time", String.format(Locale.US, "%.3f", eventTime));
             }
         } else {
             event.put("event", info);
---
+++ src/main/java/com/splunk/logging/serialization/PlainTextEventBodySerializer.java
@@ -0,0 +1,21 @@
+package com.splunk.logging.serialization;
+
+import com.splunk.logging.EventBodySerializer;
+import com.splunk.logging.HttpEventCollectorEventInfo;
+
+/**
+ * Custom serializer which send message in plain-text format and provides message timestamp with millisecond precision.
+ */
+public class PlainTextEventBodySerializer implements EventBodySerializer {
+
+	@Override
+	public String serializeEventBody(HttpEventCollectorEventInfo eventInfo, Object formattedMessage) {
+		return String.valueOf(formattedMessage);
+	}
+
+	@Override
+	public double getEventTime(HttpEventCollectorEventInfo eventInfo) {
+		return eventInfo.getTime();
+	}
+
+}
